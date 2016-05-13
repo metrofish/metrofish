c88488938e6368cf42f4931635a6ccc3b0904c59
Jorge Bucaran <mail@jorgebucaran.com>
2016-05-14 02:22:10 +0900

    Powerline prompt optimized for speed and horizontal space

---
+++ functions/fish_right_prompt.fish
@@ -0,0 +1,39 @@
+function fish_right_prompt
+    set -l status_copy $status
+    set -l status_code $status_copy
+    set -l status_color 333
+    set -l status_glyph
+    set -l duration_glyph
+
+    switch "$status_copy"
+        case 0 "$__metro_status_last"
+            set status_code
+    end
+
+    set -g __metro_status_last $status_copy
+
+    if test "$status_copy" -eq 0
+        set duration_glyph " "
+    else
+        set status_color red
+        set status_glyph │
+    end
+
+    if test "$CMD_DURATION" -gt 250
+        if test ! -z "$status_code"
+            echo -sn (set_color $status_color) "($status_code)" (set_color normal)
+            set status_glyph ┃
+        end
+
+        set -l duration (echo $CMD_DURATION | humanize_duration)
+        echo -sn (set_color $status_color) " ($duration) $duration_glyph" (set_color normal)
+
+    else
+        if test ! -z "$status_code"
+            echo -sn (set_color $status_color) "$status_code " (set_color normal)
+            set status_glyph ┃
+        end
+    end
+
+    echo -sn (set_color $status_color) "$status_glyph" (set_color normal)
+end
