e45d30aba303033a90e57a9ef96cc8132ced38f5
Andrew Kennedy <andrewk36@gmail.com>
2016-12-12 02:34:22 -0800

    vi mode support

--- fish_prompt.fish
+++ fish_prompt.fish
@@ -5,6 +5,19 @@ function fish_prompt
     set -l base
     set -l base_color 888 161616
 
+    if test "$fish_key_bindings" = "fish_vi_key_bindings"
+      switch $fish_bind_mode
+        case default
+          segment brwhite red "[N]"
+        case insert
+          segment brwhite green "[I]"
+        case replace-one
+          segment brwhite green "[R]"
+        case visual
+          segment brwhite magenta "[V]"
+      end
+    end
+
     if test "$PWD" = ~
         set base "~"
 
